6eec8b29c09ba83f1260326066fafe79c04c96bf
giorgiopeng <84867579@qq.com>
2020-02-11 16:03:14 +0800

    初步column图

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Column } from '@antv/g2plot'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { result: [] };
+  }
+  componentDidMount() {
+    let url = 'https://lab.isaaclin.cn/nCoV/api/area';
+    fetch(url)
+      .then(re => re.json())
+      .then(re => {
+        console.log(re)
+        let data = re.results.map((el) => {
+          if (el.country == '中国') {
+            return { type: el.provinceShortName, value: el.curedCount }
+          }
+          else
+            return;
+        })
+        data = data.filter(el => el !== undefined)
+        console.log(data)
+        const barPlot = new Column("canvas", {
+          data,
+          xField: 'type',
+          yField: 'value',
+
+        })
+        barPlot.render();
+        return;
+      })
+      .catch((er) => console.log(er));
+  }
+  render() {
+    return (
+      <div>
+        <div id="canvas" style={{ height: '1500px', width: '100%' }}></div>
+      </div>
+    );
+  }
 }
 
 export default App;
